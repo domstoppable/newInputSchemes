20d8437730826eb0ec01adb48c65acc28d3c008b
Dominic Canare <dom@greenlightgo.org>
2015-10-19 00:25:14 -0500

    clean up

--- main.py
+++ main.py
@@ -1,17 +1,10 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-import sys, os, time, inspect, random, subprocess
+import sys
 from PySide import QtGui, QtCore
 from DragDropUI import *
 from InputScheme import *
-
-src_dir = os.path.dirname(inspect.getfile(inspect.currentframe()))
-arch_dir = 'lib/x64' if sys.maxsize > 2**32 else 'lib/x86'
-sys.path.insert(0, os.path.abspath(os.path.join(src_dir, 'lib')))
-sys.path.insert(0, os.path.abspath(os.path.join(src_dir, arch_dir)))
-
-
 
 def main(args):
 	app = QtGui.QApplication(sys.argv)
